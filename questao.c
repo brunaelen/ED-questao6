c3aa202797b49ca78b0a820edb10e88587d33bec
Bruna Elen <95480254+brunaelen@users.noreply.github.com>
2023-10-07 10:58:39 -0300

    Update questao.c

--- questao.c
+++ questao.c
@@ -10,20 +10,20 @@ int main () {
         };
     int i, n, num;
 
-		for (i = 0; i < 4; i++) {
+	    for (i = 0; i < 4; i++) {
             for (n = 0; n < 4; n++) {
-				printf ("%d\t", matriz[i][n]);
+		printf ("%d\t", matriz[i][n]);
             }     
-		}
-        for (i = 0; i < 4; i++) {
+	    }
+            for (i = 0; i < 4; i++) {
                 printf ("\nQual e o valor que deseja buscar? ");
-				scanf ("%d", &num);
+		scanf ("%d", &num);
                 for (n = 0; n < 4; n++) {
                 
                 if (matriz[i][n]==num) {
-						printf ("%d ocorre na posicao [%d][%d] do vetor.\n", num, i, n);	
-                        return 0;	
-				}
+		    printf ("%d ocorre na posicao [%d][%d] do vetor.\n", num, i, n);	
+        	    return 0;	
+		}
                 }
         }
 }
